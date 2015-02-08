d0ac3fbba59a8844722fd8b2a5e2787ec654c5c7
xescuGC <xescugil@gmail.com>
2015-02-08 23:50:18 +0100

    Added a ChatConstatns file to use them as a name references for the Actions

---
+++ js/constants/ChatConstants.js
@@ -0,0 +1,11 @@
+var KeyMirror = require('keymirror');
+
+module.exports = {
+  ActionTypes: KeyMirror({
+    CREATE_MESSAGE: null
+  }),
+
+  PayloadSources: KeyMirror({
+    VIEW_ACTION: null
+  })
+};
