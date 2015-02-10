2c02b0eb852e80b2d43684f0e87cd0eb6c222d6b
xescuGC <xescugil@gmail.com>
2015-02-10 01:35:06 +0100

    Updatedt the Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,7 +24,30 @@ module.exports = function(grunt) {
     },
     nodemon: {
       dev: {
-        script: 'server.js'
+        script: 'server.js',
+        options: {
+          callback: function (nodemon) {
+            nodemon.on('log', function (event) {
+              console.log(event.colour);
+            });
+
+            // opens browser on initial server start
+            nodemon.on('config:update', function () {
+               //Delay before server listens on port
+              setTimeout(function() {
+                require('open')('http://localhost:35729');
+              }, 1000);
+            });
+
+            // refreshes browser when server reboots
+            nodemon.on('restart', function () {
+              // Delay before server listens on port
+              setTimeout(function() {
+                require('fs').writeFileSync('.rebooted', 'rebooted');
+              }, 1000);
+            });
+          }
+        }
       }
     },
     //connect: {
